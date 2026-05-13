83b989a37b87da541d0b5d8c9e233a43c256997e
SavedByLight <mrfluffyoven@gmail.com>
2026-05-13 11:28:03 +0100

    cleanup tree

--- device.mk
+++ device.mk
@@ -18,8 +18,7 @@ PRODUCT_PACKAGES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service \
+    android.hardware.health@2.1-impl.recovery 
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
