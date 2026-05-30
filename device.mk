24209a890d46566aed080cfb2f87c41fd52b636e
mrfluffyoven <mrfluffyoven@gmail.com>
2026-05-30 08:44:07 +0000

    fix fastboot and sideload
    Change-Id: Ica82b7c8d6a4713924f296856dc55a3c2bf7a78e

--- device.mk
+++ device.mk
@@ -14,3 +14,15 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
